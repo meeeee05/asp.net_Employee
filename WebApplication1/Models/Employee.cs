20208535a9321ff6a5fd9cacb45c1b826321f118
meeeee05 <stmi.eeeee05@gmail.com>
2023-11-16 20:57:59 +0900

    Employee first commit

---
+++ WebApplication1/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace WebApplication1.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Area { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime HireDate { get; set; }
+    }
+}
