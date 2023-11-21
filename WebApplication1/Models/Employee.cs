15d93116a566a3debb46cfca09449f04be8e4e68
meeeee05 <stmi.eeeee05@gmail.com>
2023-11-21 22:25:42 +0900

    Display属性追加

--- WebApplication1/Models/Employee.cs
+++ WebApplication1/Models/Employee.cs
@@ -3,10 +3,16 @@ namespace WebApplication1.Models
 {
     public class Employee
     {
+        [Display(Name = "id")]
         public int Id { get; set; }
+
+        [Display(Name = "名前")]
         public string Name { get; set; }
+
+        [Display(Name = "エリア")]
         public string Area { get; set; }
 
+        [Display(Name = "入社日")]
         [DataType(DataType.Date)]
         public DateTime HireDate { get; set; }
     }
